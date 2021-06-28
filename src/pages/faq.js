48953c0dac4676e84d48c8be2abc8dfa0af0278f
Karan <karanche97@gmail.com>
2021-06-28 10:28:25 +0530

    [Init] FP-DOCS

---
+++ src/pages/faq.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Layout from "@theme/Layout";
+
+export default function Faq() {
+  return (
+    <Layout
+      title="Welcome to FAQ's"
+      description="Description will go into a meta tag in <head />"
+    >
+      <h1> Welcome to FAQ </h1>
+      <p> Fieldproxy's FAQ's answered here. </p>
+    </Layout>
+  );
+}
